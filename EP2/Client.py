91db5aee922f2625773bf09d41a56b941f79519e
ccastro <caiofcastro@usp.br>
2021-05-31 11:00:25 -0300

    adds user input loop

--- EP2/Client.py
+++ EP2/Client.py
@@ -6,6 +6,13 @@ class Client:
     def __init__(self):
         self.socket = None
 
+    def start(self,port,ip):
+        """Connects to server and reads user input."""
+
+        self.connect(port,ip)
+        self.command_loop()
+        self.disconnect()
+
     def connect(self, port, ip):
         """Connect to server or to other client"""
         try:
@@ -13,12 +20,29 @@ class Client:
         except:
             print(f"Could not connect to address {ip}:{port}")
 
-        msg = bytes("Ola",'utf-8')
-        print(msg)
-        self.socket.sendmsg([msg])
-
-        self.disconnect()
-
     def disconnect(self):
         print("Closing socket")
+        
+        #Sends Empty message to indicate connection closing
+        self.socket.sendmsg([bytes()])
         self.socket.close()
+
+    def command_loop(self):
+        """Reads User commands in a loop and sends then to connection."""
+        while True:
+            cmd = input(">").split(" ")
+            print(cmd)
+
+            if cmd[0] == "exit":
+                break
+            
+            if cmd[0] == "adduser":
+                self.socket.sendmsg([
+                    bytes(cmd[0],"utf-8"),
+                    bytes(";","utf-8"),
+                    bytes(cmd[1],"utf-8"),
+                ])
+
+            
+            for s in cmd:
+                self.socket.sendmsg([bytes(s,'utf-8')])
